4b98f4dd38ad720e29cd3fc4e8fa1b26a42467b5
Maycon <mayconlimasan@gmail.com>
2026-08-16 16:47:47 -0300

    chore: bump versionCode to 11 (version 1.11) for Google Play release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "io.github.engsoft7.calcdroid"
         minSdk = 24
         targetSdk = 37
-        versionCode = 10
-        versionName = "1.10"
+        versionCode = 11
+        versionName = "1.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
