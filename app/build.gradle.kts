6bb249a8023a8449ef43326357a5f8bcabdb4780
Maycon <mayconlimasan@gmail.com>
2026-08-16 16:43:39 -0300

    chore: bump versionCode to 10 (version 1.10) for Google Play release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "io.github.engsoft7.calcdroid"
         minSdk = 24
         targetSdk = 37
-        versionCode = 9
-        versionName = "1.9"
+        versionCode = 10
+        versionName = "1.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
